cc48eb6e05aadd8b557d7e9d51dec88c8e3372ef
igautham <33679217+igautham@users.noreply.github.com>
2020-09-09 14:05:41 +0530

    Delete route_constants.js

--- restapi/route_constants.js
+++
@@ -1,8 +0,0 @@
-module.exports.ROUTE_CONSTANTS = {
-    DEFAULT: "/",
-    GET_LOGIN: "/login",
-    GET_REGISTER: "/register",
-    GET_PRODUCTS: "/products",
-    POST_REGISTER: "/register",
-    POST_LOGIN: "/login"
-}
