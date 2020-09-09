78b5e430d4243815f039dc242d5977a4cf372691
igautham <33679217+igautham@users.noreply.github.com>
2020-09-09 14:05:13 +0530

    Add files via upload

---
+++ restapi/route_constants.js
@@ -0,0 +1,8 @@
+module.exports.ROUTE_CONSTANTS = {
+    DEFAULT: "/",
+    GET_LOGIN: "/login",
+    GET_REGISTER: "/register",
+    GET_PRODUCTS: "/products",
+    POST_REGISTER: "/register",
+    POST_LOGIN: "/login"
+}
